f4c983358c52abcd6fa24f82707c6cfec06e35ee
TehilaRamati <109224083+TehilaRamati@users.noreply.github.com>
2022-07-13 19:24:50 +0300

    Create solution1.py

---
+++ solution1.py
@@ -0,0 +1,91 @@
+
+def replace_formula(curr, arr):
+    res = ''
+    open = curr.count("{")
+    close = curr.count("}")
+    if open == close:
+        if open > 0:
+            while open > 0:
+                open -= 1
+                a = curr.split("{")[1]
+                b = a.split("}")[0]
+                res = curr.replace("{" + b + "}", arr[int(b)])
+        else:
+            res = str(curr)
+    else:
+        raise Exception ("Invalid formula")
+    return res
+
+
+def calculator(action, a, b):
+    # switcher={
+    #     '+': num + = n[j + 1]
+    # }
+    # return switcher.get(num, None)
+    if action == '+':
+        return str(a + b)
+    if action == '-':
+        return str(a - b)
+    if action == '*':
+        return str(a * b)
+    if action == '/':
+        return str(a / b)
+    raise Exception("invalid action")
+
+
+def calc(arr):
+    res = ''
+    index = 0
+    while index < len(arr):
+        if not arr[index].isdigit():
+            res = calculator(arr[index], int(res), int(arr[index + 1]))
+            index += 1
+        else:
+            res = res + arr[index]
+        index += 1
+    return res
+
+
+def print_list(input_array):
+    index = 0
+    while index < len(input_array):
+        print("[{}: {}]".format(index, input_array[index]), end=" ")
+        index += 1
+    print("\n")
+
+
+def prepare_array(input_list):
+
+    orgin_list = input_list.copy()
+
+    index = 0
+    while index < len(input_list):
+        import re
+        curr = replace_formula(input_list[index], input_list)
+        curr = re.split('(\W+)', curr)
+        curr = calc(curr)
+        input_list[index] = curr
+        index += 1
+    print_list(input_list)
+    return orgin_list
+
+
+def change_value(input_array, cell, value):
+    input_array[cell] = str(value)
+    org_array = prepare_array(input_array)
+    org_array[cell] = str(value)
+    return org_array
+
+
+if __name__ == '__main__':
+    # input = "2,18,=2*{0},9,={2}+1*5"
+    input = "2,18,=20*{0}*{0},9,={2}+1*50"
+    print("#Input_file: \n{}".format(input))
+    input_array = input.replace('=', '').split(',')
+
+    print("#a:")
+    input_arr = prepare_array(input_array)
+    print("#b:")
+    input_arr = change_value(input_arr, 0, 3)
+    print("#c:")
+    change_value(input_arr, 2, 1)
